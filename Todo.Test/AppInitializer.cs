71e9decd4453602576187d45f3710abee5148631
ATMIRA\agustin.rojas <agustin.rojas@atmira.com>
2018-10-02 12:36:48 +0200

    commit project

---
+++ Todo.Test/AppInitializer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace Todo.Test
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    .InstalledApp("Todo.Android")
+                    .PreferIdeSettings()
+                    .EnableLocalScreenshots()
+                    .StartApp();
+            }
+
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
---
+++ Todo.Test/Tests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace Todo.Test
+{
+    [TestFixture(Platform.Android)]
+    //[TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        static readonly Func<AppQuery, AppQuery> SaveButton = c => c.Button().Marked("ButtonSave");
+        static readonly Func<AppQuery, AppQuery> NewElement = c => c.Text("Hoooooola");
+
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void WelcomeTextIsDisplayed()
+        {
+           // app.Repl();
+            app.Tap("Add");
+            app.Tap("NameText");
+            app.EnterText("Hoooooola");
+            app.Tap("NotesText");
+            app.EnterText("Nooooootes");
+            app.DismissKeyboard();
+            app.Tap(SaveButton);
+
+            app.Tap(NewElement);
+            app.Tap("CheckDone");
+            app.Tap("ButtonSave");
+            app.Back();
+        }
+    }
+}
